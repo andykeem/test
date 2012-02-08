87d198f861eab6c2227a4c284414259e9c42bbfb
foo <foo@bar.net>
2012-02-08 06:08:02 -0500

    header and footer included

--- index.php
+++ index.php
@@ -3,4 +3,9 @@
 $title = 'Home Page';
 
 ?>
+
+<?php require_once(realpath(dirname(__FILE__) . '/include/header.php')) ?>
+
 <p><?php echo $title ?></p>
+
+<?php require_once(realpath(dirname(__FILE__) . '/include/footer.php')) ?>
